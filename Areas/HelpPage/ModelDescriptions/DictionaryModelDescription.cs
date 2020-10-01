64887c25c87749c366d3dd3d07acd592a44898d0
ME0ny <ipatov.987654321@gmail.com>
2020-10-01 21:29:07 +1000

    Добавьте файлы проекта.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TelegramCalcBot.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Models/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelegramCalcBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://telegramcalcbot.azurewebsites.net:443/{0}";
+
+        public static string Name { get; set; } = "Ipatoff_bot";
+
+        public static string Key { get; set; } = "";
+
+    }
+}
